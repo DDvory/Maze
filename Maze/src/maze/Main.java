9be50da1c196dc1b85b45e02b807315c043a7eec
BuildTools <unconfigured@null.spigotmc.org>
2018-03-27 21:24:45 +0200

    Merge origin/Development into Development
    Conflicts:
    	Maze/src/maze/Main.java

--- Maze/src/maze/Main.java
+++ Maze/src/maze/Main.java
@@ -15,8 +15,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("DDvory je čuráááák");
-        System.out.println("Mustul je kokot");
+       
     }
     
 }
